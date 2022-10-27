45f516d9c8f9af5a369c057f877a0e62c979686c
Alexzanah G <algr4177@colorado.edu>
2022-10-27 17:03:19 +0000

    rand int NxM

--- data_processor.py
+++ data_processor.py
@@ -1,7 +1,11 @@
-# remember to import your libraries!
+import numpy as np
+import random
+
 
 def get_random_matrix(num_rows, num_columns):
-	return None
+	np.random.randint(0,1)
+    num_rows
+    return None
 
 def get_file_dimensions(file_name):
 	return (0,0)
