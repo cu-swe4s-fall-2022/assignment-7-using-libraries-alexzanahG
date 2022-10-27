ea934260df3d330d6e59a5af76dc4bc4300afb31
Alexzanah G <algr4177@colorado.edu>
2022-10-27 22:54:44 +0000

    fix?

--- data_processor.py
+++ data_processor.py
@@ -4,23 +4,20 @@ import random
 
 
 def get_random_matrix(num_rows, num_cols):
-    matrix = np.random.uniform( low = 0.0, high = 1.0, size = (num_rows,num_cols))
-    
-    return matrix 
-
+    matrix = np.random.uniform( low = 0.0,
+                               high = 1.0,
+                               size = (num_rows,num_cols))
+    return matrix
 def get_file_dimensions(file_name):
-    df = pd.read_csv(file_name, 
-                    sep = ",", header=False)
+    df = pd.read_csv(file_name,
+                    sep = ",",
+                    header=False)
     df.size(num_rows, num_cols)
-    
     return (0,0)
-
 def write_matrix_to_file(num_rows, num_columns, file_name):
-    matrix = np.random.uniform(low = 0.0, 
-                       high = 1.0, 
+    matrix = np.random.uniform(low = 0.0,
+                       high = 1.0,
                        size = (num_rows, num_cols))
     fn = pd.DataFrame(file_name)
     fn.to_csv(new_file_name.csv)
-   
     return new_file_name
-    
