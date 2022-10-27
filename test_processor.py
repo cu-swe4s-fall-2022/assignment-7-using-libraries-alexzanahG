b4c4b00486dfd97e894de11aaad274c070549bb5
Alexzanah G <algr4177@colorado.edu>
2022-10-27 22:55:17 +0000

    fix?

--- test_processor.py
+++ test_processor.py
@@ -5,16 +5,16 @@ import numpy as np
 import os
 
 class TestUtils(unittest.TestCase):
-    test_list= [1,3,5,4,6,]
+    test_list= [1, 3, 5, 4, 6]
     
     @classmethod
     def setUpClass(cls):
-        cls.test_list= [1,3,5,4,6,]
+        cls.test_list= [1, 3, 5, 4, 6]
         cls.rand_list= []
         for i in range (15):
-            rand_int= random.randint(0,14)
+            rand_int= random.randint(0, 14)
             cls.rand_list.append(rand_int)
-            
+
     @classmethod
     def tearDownClass(cls):
         cls.test_list= None
@@ -23,15 +23,11 @@ class TestUtils(unittest.TestCase):
 
     def test_to_start(self):
         self.assertTrue(True)
-    
-    def get_random_matrix(self):
-        #test for matrix
-        self.assertTrue(dp.get_random_matrix())
-        
-    def get_file_dimensions(self):
 
-    def write_matrix_to_file(self):
-        
-        
+    def matrix(self):
+        #test for matrix
+        self.assertTrue(dp.get_random_matrix(range (0.0, 1.0)))
+        self.assertFalse(dp.get_random_matrix( 
+   
 if __name__ == '__main__':
     unittest.main()
