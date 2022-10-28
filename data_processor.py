caa934b30c80ad3e885c393543ea818720dfd859
Alexzanah G <algr4177@colorado.edu>
2022-10-28 00:03:36 +0000

    final func

--- data_processor.py
+++ data_processor.py
@@ -3,7 +3,10 @@ import pandas as pd
 import random
 
 
-def get_random_matrix(num_rows, num_cols):
+def get_random_matrix(num_rows,
+                      num_cols,
+                      seed):
+    np.random.seed(seed)
     matrix = np.random.uniform( low = 0.0,
                                high = 1.0,
                                size = (num_rows,num_cols))
@@ -11,13 +14,19 @@ def get_random_matrix(num_rows, num_cols):
 def get_file_dimensions(file_name):
     df = pd.read_csv(file_name,
                     sep = ",",
-                    header=False)
-    df.size(num_rows, num_cols)
-    return (0,0)
-def write_matrix_to_file(num_rows, num_columns, file_name):
-    matrix = np.random.uniform(low = 0.0,
-                       high = 1.0,
-                       size = (num_rows, num_cols))
-    fn = pd.DataFrame(file_name)
-    fn.to_csv(new_file_name.csv)
-    return new_file_name
+                    header= None)
+    file_dim=df.shape
+    return file_dim
+def write_matrix_to_file(num_rows,
+                         num_cols,
+                         file_name,
+                         seed):
+    np.random.seed(seed)
+    matrix= get_random_matrix(num_rows,
+                              num_cols,
+                              seed)
+    matrix_file = np.savetxt(file_name,
+                            matrix,
+                            delimiter= ',')
+    
+    return matrix_file
